cf9f427e1db0355d3f7c45aab00da018a1e3d176
Emanuelle Guedes <emanuelle.guedes.ext@synchro.com.br>
2026-03-20 18:01:46 -0300

    feat: adicionando calculadora no index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,225 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function Index() {
+  const [currentNumber, setCurrentNumber] = useState<string>("");
+  const [lastNumber, setLastNumber] = useState<string>("");
 
-export default function HomeScreen() {
+  function handleInput(buttonPressed: string | number) {
+    const value = String(buttonPressed);
+
+    if (["*", "/", "+", "-", "ˆ"].includes(value)) {
+      setCurrentNumber(currentNumber + " " + value + " ");
+      return;
+    }
+
+    if (value === "<") {
+      setCurrentNumber(currentNumber.slice(0, -1));
+      return;
+    }
+
+    if (value === "C") {
+      setCurrentNumber("");
+      setLastNumber("");
+      return;
+    }
+
+    if (value === ",") {
+      setCurrentNumber(currentNumber + ".");
+      return;
+    }
+
+    if (value === "=") {
+      setLastNumber(currentNumber + " =");
+      calculator();
+      return;
+    }
+
+    setCurrentNumber(currentNumber + value);
+  }
+
+  function calculator() {
+    try {
+      const splitNumbers = currentNumber.split(" ");
+      const operator = splitNumbers[1];
+
+      const num1 = parseFloat(splitNumbers[0]);
+      const num2 = parseFloat(splitNumbers[2]);
+
+      let result = 0;
+
+      switch (operator) {
+        case "*":
+          result = num1 * num2;
+          break;
+        case "/":
+          result = num1 / num2;
+          break;
+        case "+":
+          result = num1 + num2;
+          break;
+        case "-":
+          result = num1 - num2;
+          break;
+        case "ˆ":
+          result = num1 ** num2;
+          break;
+      }
+
+      setCurrentNumber(result.toString());
+    } catch {
+      setCurrentNumber("Erro");
+    }
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View style={styles.container}>
+      <View style={styles.result}>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultText}>{currentNumber || "0"}</Text>
+      </View>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("C")}>
+          <Text style={styles.textButton}>C</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("ˆ")}>
+          <Text style={styles.textButton}>xˆy</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("<")}>
+          <Text style={styles.textButton}>{"<"}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("/")}>
+          <Text style={styles.textButton}>/</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(7)}>
+          <Text style={styles.textButton}>7</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(8)}>
+          <Text style={styles.textButton}>8</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(9)}>
+          <Text style={styles.textButton}>9</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("*")}>
+          <Text style={styles.textButton}>*</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(4)}>
+          <Text style={styles.textButton}>4</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(5)}>
+          <Text style={styles.textButton}>5</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(6)}>
+          <Text style={styles.textButton}>6</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("-")}>
+          <Text style={styles.textButton}>-</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(1)}>
+          <Text style={styles.textButton}>1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(2)}>
+          <Text style={styles.textButton}>2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(3)}>
+          <Text style={styles.textButton}>3</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("+")}>
+          <Text style={styles.textButton}>+</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#808080" }]}
+          onPress={() => handleInput("=")}
+        >
+          <Text style={[styles.textButton, { color: "black", fontSize: 30 }]}>
+            =
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(0)}>
+          <Text style={styles.textButton}>0</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(",")}>
+          <Text style={styles.textButton}>,</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#808080" }]}
+          onPress={() => handleInput("=")}
+        >
+          <Text style={[styles.textButton, { color: "black", fontSize: 30 }]}>
+            =
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    padding: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  result: {
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: "100%",
+    height: 250,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  resultText: {
+    fontSize: 70,
+    fontWeight: "200",
+    margin: 10,
+    alignSelf: "flex-end",
+  },
+
+  historyText: {
+    fontSize: 20,
+    marginRight: 10,
+    alignSelf: "flex-end",
+    color: "gray",
+  },
+
+  buttons: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+
+  button: {
+    width: "25%",
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ededed",
+  },
+
+  textButton: {
+    fontSize: 25,
   },
 });
